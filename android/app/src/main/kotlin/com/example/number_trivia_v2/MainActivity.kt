1030503025a7323ef07c06eb22e3a7031714516d
Md. Yousuf Ali <myali@ael-bd.com>
2021-12-27 12:01:14 +0600

    initial commit

---
+++ android/app/src/main/kotlin/com/example/number_trivia_v2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.number_trivia_v2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import './features/presentation/pages/number_trivia_page.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const NumberTriviaPage(),
+    );
+  }
+}
